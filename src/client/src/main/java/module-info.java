030430693390a006686112a33dc248a1ac4d2c55
Nguyen Thanh Nam <namnguyen160504@gmail.com>
2024-10-26 16:18:52 +0700

    Init Javafx

---
+++ src/client/src/main/java/module-info.java
@@ -0,0 +1,17 @@
+module mop.app.client {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    requires static lombok;
+
+    opens mop.app.client to javafx.fxml;
+    exports mop.app.client;
+}
---
+++ src/server/src/main/java/module-info.java
@@ -0,0 +1,17 @@
+module mop.app.server {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    requires static lombok;
+
+    opens mop.app.server to javafx.fxml;
+    exports mop.app.server;
+}
